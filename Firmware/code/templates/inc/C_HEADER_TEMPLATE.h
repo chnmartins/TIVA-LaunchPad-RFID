971b8a872b0d6a7a64cb7d8e80a34b20cc452dc2
diegomg <dmartinezgarcia@outlook.com>
2015-02-10 18:42:54 +0100

    Initial Project.

---
+++ Firmware/code/templates/inc/C_HEADER_TEMPLATE.h
@@ -0,0 +1,39 @@
+/**
+  ******************************************************************************
+  * @file    C_HEADER_TEMPLATE.h
+  * @author  Header template author and its contact email
+  * @version Version 1 Issue 1
+  * @date    Date when the file was released.	(i.e. 05-March-2014)
+  * @brief   Short description about the header template.
+  ******************************************************************************
+  * @attention
+  *
+  *		Copyright Information (C)
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __C_HEADER_TEMPLATE_H
+#define __C_HEADER_TEMPLATE_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+
+/* Exported macro ------------------------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__C_HEADER_TEMPLATE_H */
+ 
---
+++ Firmware/code/templates/src/C_SOURCE_TEMPLATE.c
@@ -0,0 +1,32 @@
+/**
+  ******************************************************************************
+  * @file    C_SOURCE_TEMPLATE.C
+  * @author  Source template author and its contact email
+  * @version Version 1 Issue 1
+  * @date    Date when the file was released.	(i.e. 05-March-2014)
+  * @brief   Short description about the functionalities this file provides.
+  *  @verbatim
+  *			Full information on how to use this driver.
+  *  @endverbatim        
+  *
+  ******************************************************************************
+  * @attention
+  *		
+  *		(Copyright Information)
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
